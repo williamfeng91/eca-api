af4acc0d859f4621a73f149aa6d8d94140f1eece
William Feng <williamf@webrecs.com.au>
2015-10-20 09:42:57 +1100

    Added swagger UI and updated API

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var SwaggerExpress = require('swagger-express-mw');
+var SwaggerUi = require('swagger-tools/middleware/swagger-ui');
 var app = require('express')();
 module.exports = app; // for testing
 
@@ -11,13 +12,12 @@ var config = {
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
 
+  // Add swagger-ui (This must be before swaggerExpress.register)
+  app.use(SwaggerUi(swaggerExpress.runner.swagger));
+
   // install middleware
   swaggerExpress.register(app);
 
   var port = process.env.PORT || 10010;
   app.listen(port);
-
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
 });
